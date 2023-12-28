70f26592d9089079ae22f093c509f759d8824099
MarshyMelloow <76271349+MarshyMelloow@users.noreply.github.com>
2023-12-28 14:44:59 -0600

    Update init.d.sh

--- init.d.sh
+++ init.d.sh
@@ -1,10 +1,20 @@
-sudo mkdir -p /opt/mellowservices && cd /opt/mellowservices && \
-curl -O https://raw.githubusercontent.com/MellowServices/snailycadv4-Installscript/main/snailyinstall.sh && \
-chmod +x snailyinstall.sh && \
-SCRIPT_PATH="/opt/mellowservices/snailyinstall.sh" && \
-INITD_SCRIPT="/etc/init.d/snailycad" && \
-sudo cp "$SCRIPT_PATH" "$INITD_SCRIPT" && \
-sudo chmod +x "$INITD_SCRIPT" && \
-sudo update-rc.d snailycad defaults && \
-sudo service snailycad start && \
-echo "SnailyCAD installation script has been downloaded to /opt/mellowservices, converted, and the init.d script has been created and started."
+#!/bin/bash
+
+SERVICE_FILE="/etc/systemd/system/snailycad.service"
+
+cat <<EOF | sudo tee "$SERVICE_FILE"
+[Unit]
+Description=SnailyCAD Startup Script
+
+[Service]
+Type=simple
+ExecStart=/opt/mellowservices/snailyinstall.sh
+TimeoutStartSec=7200
+
+[Install]
+WantedBy=default.target
+EOF
+
+sudo update-rc.d snailycad defaults
+sudo systemctl enable snailycad
+sudo systemctl start snailycad
